b5c50711cb3de2a595b363134526d7464f484efb
deepak <deepakbaradwaj933@gmail.com>
2024-04-13 18:57:03 +0530

    a

--- home.php
+++ home.php
@@ -634,6 +634,20 @@ if (!$city_result) {
                     </div>
                 </a>
             </div>
+            <div class="grid-items">
+                <h3>Best Call girl in City <span>"P"</span></h3>
+                <a href="https://ctchicks.com/escorts/pune/">
+                    <div class="item-city-name">
+                        <div class="location-logo"><img src="<?=get_url() ?>home/assets/loca.png" width="100%" height="100%" alt=""></div>
+                        <div class="name_city">
+                            <p>Pune (<?php $sql = "SELECT COUNT(profile_id) as a FROM profiles WHERE callgirl_escort = 'escorts' && cities = 'pune'";
+                                                                                                                        $res = mysqli_query($con, $sql);
+                                                                                                                        $row = mysqli_fetch_array($res);
+                                                                                                                        echo $row['a'] ?>)</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
 
 
