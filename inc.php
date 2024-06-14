a8e3d4541716dc04fa41dcf04f4cb70232c93057
deepak <deepakbaradwaj933@gmail.com>
2024-06-15 00:54:18 +0530

    b

--- inc.php
+++ inc.php
@@ -1,4 +1,11 @@
 <?php 
+Header set X-Content-Type-Options: "nosniff"
+Header set X-Frame-Options: "DENY"
+Header set X-XSS-Protection: "1; mode=block"
+Header set Content-Security-Policy "default-src 'self'"
+
+
+
 include './routes.php';
 include './common.php';
 include './connection.php';
