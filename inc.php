fa6dc80cc58ad794f8e74773e733c6ffeb266a3b
deepak <deepakbaradwaj933@gmail.com>
2024-06-15 00:54:49 +0530

    b

--- inc.php
+++ inc.php
@@ -1,8 +1,4 @@
 <?php 
-Header set X-Content-Type-Options: "nosniff"
-Header set X-Frame-Options: "DENY"
-Header set X-XSS-Protection: "1; mode=block"
-Header set Content-Security-Policy "default-src 'self'"
 
 
 
