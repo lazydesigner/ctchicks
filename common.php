907ce21f4bfaf4e06aa8d199f78eb139c17a45dc
deepak <deepakbaradwaj933@gmail.com>
2024-09-30 16:50:41 +0530

    new domain

--- common.php
+++ common.php
@@ -21,6 +21,7 @@ $noindex = '<meta name="robots" content="index, follow, max-snippet:-1, max-vide
     ';
 $page_css = ' 
 <meta name="google-site-verification" content="wzD7JyCNBsUVl3FL5qJaRALaIosrEQZYgsaCFcp9lhg" />
+<meta name="google-site-verification" content="Y9mi4SSPVVZuOFNz9ADeEyzpN8mG7UPYkkiIPy2y9bg" />
 <!--<link rel="stylesheet" href="'.get_url().'assets/css/navbar.css" async>-->
 <link as="style"  rel="preload" href="'.get_url().'fonts/remixicon.css">
 
